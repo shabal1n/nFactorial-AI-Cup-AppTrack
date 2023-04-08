9e32006d4969346e53a03c9b65d810d18a007943
shabal1n <artur.shab@yandex.ru>
2023-04-08 15:28:51 +0600

    Added a deisgn for the budget and expenses

---
+++ my-finance/src/App.js
@@ -0,0 +1,26 @@
+import Container from "react-bootstrap/Container"
+import { Button, Stack } from "react-bootstrap"
+import BudgetCard from "./components/BudgetCard"
+
+function App() {
+  return <Container className="my-4">
+    <Stack direction="horizontal" gap="2" className="mb-4">
+      <h1 className="me-auto">Budgets</h1>
+      <Button variant="primary">Add Budget</Button>
+      <Button variant="outline-primary">Add Expense</Button>
+    </Stack>
+    <div style={
+        { 
+          display: "grid", 
+          gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))", 
+          gap: "1rem", 
+          alignItems: "flex-start" 
+        }
+      }
+    >
+      <BudgetCard name="Entartainment" amount={5600} max={29000}></BudgetCard>
+    </div>
+  </Container>
+}
+
+export default App
